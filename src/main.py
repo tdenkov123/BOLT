e316353292d661141ef97c664b581b5d2ff98e91
TDENKOV <tdenkov123@mail.ru>
2026-01-24 18:49:10 +0300

    testing main

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 import asyncio
 
-from core.FBs.FBootLoader import FBootLoader
+from core.FunctionBlockLoader import FunctionBlockLoader
 
 
 async def main() -> None:
-    loader = FBootLoader()
-    loader.getFBList(["INT2INT"])  # supply only the blocks you need
-    fb_classes = loader.loadFBList()
+    loader = FunctionBlockLoader()
+    fb_classes = loader.loadFBList(["core.FBs.INT2INT", "core.FBs.ADD_2"])
 
     example_fb = fb_classes["INT2INT"]("ExampleFB")
     result = example_fb.execute(5)
     print(f"INT2INT execute result: {result}")
 
+    a = fb_classes["INT2INT"]("FB1")
+    b = fb_classes["ADD_2"]("FB2")
 
 if __name__ == "__main__":
     asyncio.run(main())

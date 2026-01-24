539e34307ff21ba039372cf4cffff3a5332e3334
TDENKOV <tdenkov123@mail.ru>
2026-01-24 18:49:04 +0300

    added string2string and print_console fbs

--- src/core/FBs/STRING2STRING.py
+++ src/core/FBs/STRING2STRING.py
@@ -1,11 +1,11 @@
 from core.BaseFb import BaseFB
 
-class PRINT_CONSOLE(BaseFB):
+class STR2STR(BaseFB):
     def __init__(self, name: str):
         super().__init__(name)
 
-    def execute(self, inputValue: str) -> None:
-        print(inputValue)
+    def execute(self, inputValue: str) -> int:
+        return str(inputValue)
 
     def getEventOutputs(self):
         return ["CNF"]

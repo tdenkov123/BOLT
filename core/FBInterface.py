963a2003e48451cf40e834e505d88e9a7b9803a4
TDENKOV <tdenkov123@mail.ru>
2026-02-09 17:02:11 +0300

    FBInterface id getters

--- core/FBInterface.py
+++ core/FBInterface.py
@@ -10,4 +10,26 @@ class FBInterface:
     do_names: tuple[str, ...] = ()
     do_types: tuple[Type, ...] = ()
 
-    
+    def get_ei_id(self, name: str) -> int | None:
+        try:
+            return self.ei_names.index(name)
+        except ValueError:
+            return None
+
+    def get_eo_id(self, name: str) -> int | None:
+        try:
+            return self.eo_names.index(name)
+        except ValueError:
+            return None
+
+    def get_di_id(self, name: str) -> int | None:
+        try:
+            return self.di_names.index(name)
+        except ValueError:
+            return None
+
+    def get_do_id(self, name: str) -> int | None:
+        try:
+            return self.do_names.index(name)
+        except ValueError:
+            return None

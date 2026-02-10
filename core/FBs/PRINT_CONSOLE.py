d221da4e6d6a08da6e0ed6edbdd7b1e6c0be65e7
TDENKOV <tdenkov123@mail.ru>
2026-02-10 17:14:16 +0300

    new fb design

--- core/FBs/PRINT_CONSOLE.py
+++ core/FBs/PRINT_CONSOLE.py
@@ -1,13 +1,31 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from core.datatypes.IEC_ANY import IEC_ANY
 from core.BaseFunctionBlock import BaseFunctionBlock
+from core.FBInterface import FBInterface
+
+if TYPE_CHECKING:
+    from core.ECET import EventChainExecutionThread
+
 
 class PRINT_CONSOLE(BaseFunctionBlock):
-    def __init__(self, name: str):
-        super().__init__(name)
 
-    async def execute(self, event: str):
-        if event == "REQ":
-            val = self._inputs.get("IN")
-            print(val)
-            self._outputs = {}
-            return self._outputs, ["CNF"]
-        return {}, []
+    FBINTERFACE = FBInterface(
+        ei_names=("REQ",),
+        eo_names=("CNF",),
+        di_names=("IN",),
+        di_types=(IEC_ANY,),
+    )
+
+    _EI_REQ = 0
+    _EO_CNF = 0
+    _DI_IN = 0
+
+    def execute_event(self, ei_id: int, ecet: EventChainExecutionThread) -> None:
+        if ei_id == self._EI_REQ:
+            print(self._di_vars[self._DI_IN].value)
+            self.send_output_event(self._EO_CNF, ecet)
+
+    def set_initial_values(self) -> None:
+        pass
--- core/FBs/START.py
+++ core/FBs/START.py
@@ -1,8 +1,25 @@
-from core.BaseFunctionBlock import BaseFunctionBlock
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from core.BaseFunctionBlock import BaseFunctionBlock, EXTERNAL_EVENT_ID
+from core.FBInterface import FBInterface
+
+if TYPE_CHECKING:
+    from core.ECET import EventChainExecutionThread
+
 
 class START(BaseFunctionBlock):
-    def __init__(self, name: str):
-        super().__init__(name)
 
-    async def execute(self, event: str):
-        return {}, ["START"]
+    FBINTERFACE = FBInterface(
+        eo_names=("START",),
+    )
+
+    _EO_START = 0
+
+    def execute_event(self, ei_id: int, ecet: EventChainExecutionThread) -> None:
+        if ei_id == EXTERNAL_EVENT_ID:
+            self.send_output_event(self._EO_START, ecet)
+
+    def set_initial_values(self) -> None:
+        pass

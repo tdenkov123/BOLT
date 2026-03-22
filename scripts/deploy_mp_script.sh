e3f489b7d41acae7412f453cacd444eca1e6389d
TDENKOV <tdenkov123@mail.ru>
2026-03-22 21:15:49 +0300

    deploy script update

--- scripts/deploy_mp_script.sh
+++ scripts/deploy_mp_script.sh
@@ -9,6 +9,7 @@ echo "[INFO] Deploying to $PORT from $SRC_DIR ..."
 
 mpremote connect "$PORT" cp "$SRC_DIR/boot.py" :boot.py
 mpremote connect "$PORT" cp "$SRC_DIR/mqtt_client.py" :mqtt_client.py
+mpremote connect "$PORT" cp "$SRC_DIR/mdns_discovery.py" :mdns_discovery.py
 mpremote connect "$PORT" cp "$SRC_DIR/main.py" :main.py
 mpremote connect "$PORT" cp "$SRC_DIR/config.py" :config.py
 
